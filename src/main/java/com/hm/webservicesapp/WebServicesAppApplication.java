c204754d5fbce627d70ecec6722cc534ea4efa27
madhuvayya <madhu.vayya@gmail.com>
2021-02-01 16:14:51 +0530

    Initialized project setup

---
+++ src/main/java/com/hm/webservicesapp/WebServicesAppApplication.java
@@ -0,0 +1,13 @@
+package com.hm.webservicesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesAppApplication.class, args);
+	}
+
+}
