bdabe2e010e5ccc48255d4696dc6bb35f66ad224
madhuvayya <madhu.vayya@gmail.com>
2021-02-02 22:19:10 +0530

    Added:MachineImage model class

---
+++ src/main/java/com/hm/webservicesapp/models/MachineImage.java
@@ -0,0 +1,32 @@
+package com.hm.webservicesapp.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class MachineImage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long machineImageId;
+	
+	@Column(name = "machine_Image_name", nullable = false)
+	public String machineName;
+	
+	@Column(columnDefinition = "text", name = "machine_image_desc")
+	public String machineDescription;
+	
+	@Column(name = "is_free_tier", nullable = false)
+	public String isFreeTier;
+	
+//	@Column(name = "supported_instances")
+//	public List<String> instances; 
+}
